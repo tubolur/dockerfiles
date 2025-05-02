873dbd8aa5dba8aa655188c7ae13c2906ec12071
tubolur <chugurutop987@proton.me>
2025-05-02 21:35:28 +0000

    Update debian_install.sh

--- debian_install.sh
+++ debian_install.sh
@@ -1,1 +1,2 @@
-sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+#sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+sudo apt install docker.io
